bd3101b5d61b6dbbf9928c9058ee2b579c262a77
Zainab Rizvi <szainabr@gmail.com>
2025-02-13 04:49:18 -0800

    Initial Commit

---
+++ recipes-app/recipes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipes_appApp.swift
+//  recipes-app
+//
+//  Created by Zainab Rizvi on 13/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct recipes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
